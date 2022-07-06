393a95bd21cbab8c4cdb69dde805b9059cb3b4f8
StevenEung <lllshallldowlll@gmail.com>
2022-07-06 12:55:25 +0800

    Remove public folder

--- public/assets/scripts/jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.nav-bar_desktop').find('.nav-item').hover(function (){
-        $(this).find('.dropdown-button_desktop').children().toggleClass('none');
-    }, function(){
-        $(this).find('.dropdown-button_desktop').children().toggleClass('none');
-    });
-});
